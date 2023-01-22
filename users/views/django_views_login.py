805a32df16a67e7b73283abff9287fdd5e388758
Sergei_L <lubeshkosergei@gmail.com>
2023-01-22 22:43:02 +0300

    feat:: add login and registration. html

--- users/views/django_views_login.py
+++ users/views/django_views_login.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.forms.django_views_form import UserLoginForm
 
 
 def login(request):
-    context ={
-        'form': UserLoginForm()
-    }
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username, password)
+            if user:
+                auth.login(request, user)  # +разрешения
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context=context)
 
 
